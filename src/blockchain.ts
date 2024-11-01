39bf3238e4d66d058dfc5b8c4b1f65421a6464ad
Jorge Lara <jelch2002@gmail.com>
2024-10-31 20:08:10 -0600

    feat: use base url

--- src/blockchain.ts
+++ src/blockchain.ts
@@ -3,7 +3,7 @@ import { BlockData } from "./types";
 
 export const getBlock = async (blockNumber: number): Promise<BlockData> => {
   const { data } = await axios.get(
-    `http://nanas.sh:9920/getblock/${blockNumber}`
+    `http://${process.env.RPC_BASE_URL}/getblock/${blockNumber}`
   );
   return data;
 };
