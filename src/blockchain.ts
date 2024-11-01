ba89ab3ce302f1db6fd53e9e14014c005f2b100e
Jorge Lara <jelch2002@gmail.com>
2024-10-31 20:08:48 -0600

    fix

--- src/blockchain.ts
+++ src/blockchain.ts
@@ -3,7 +3,7 @@ import { BlockData } from "./types";
 
 export const getBlock = async (blockNumber: number): Promise<BlockData> => {
   const { data } = await axios.get(
-    `http://${process.env.RPC_BASE_URL}/getblock/${blockNumber}`
+    `${process.env.RPC_BASE_URL}/getblock/${blockNumber}`
   );
   return data;
 };
