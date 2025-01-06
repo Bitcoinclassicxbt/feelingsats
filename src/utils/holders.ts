f4f10238ba60586c971121f559418476d1b4e1a4
Jorge Lara <jelch2002@gmail.com>
2025-01-05 19:28:49 -0600

    fix

--- src/utils/holders.ts
+++ src/utils/holders.ts
@@ -9,15 +9,20 @@ export type IHolder = {
 
 export const getHolders = async (models: Models) => {
   try {
-    const UtxoResponse = await models.Utxo.findAll({
-      attributes: [
-        "address",
-        [models.sequelize.fn("SUM", models.sequelize.col("amount")), "balance"],
-      ],
-      group: ["address"],
-      order: [[models.sequelize.literal("balance"), "DESC"]],
-      raw: true,
-    });
+    const UtxoResponse = (
+      await models.Utxo.findAll({
+        attributes: [
+          "address",
+          [
+            models.sequelize.fn("SUM", models.sequelize.col("amount")),
+            "balance",
+          ],
+        ],
+        group: ["address"],
+        order: [[models.sequelize.literal("balance"), "DESC"]],
+        raw: true,
+      })
+    ).filter((utxo: any) => utxo.address.length > 0);
 
     const holders: IHolder[] = UtxoResponse.map((utxo: any, index: number) => ({
       address: utxo.address,
